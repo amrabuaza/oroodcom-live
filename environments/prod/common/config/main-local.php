5267fa9846113dc39bdeb5e30425fc1ad7df3968
amrabuaza <amr@insalat.com>
2020-05-30 01:33:17 +0300

    update database connection on serve

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=oroodcom_db',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=localhost;dbname=oroodcom',
+            'username' => 'oroodcom_root',
+            'password' => 'sM0a1n8zW3h;',
             'charset' => 'utf8',
         ],
         'mailer' => [
